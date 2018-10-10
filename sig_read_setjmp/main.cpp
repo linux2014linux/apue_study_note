f6d6c6003ce2305d5c1edbc116e5c79067ed2ddb
linux2014linux <2319179516@qq.com>
2018-10-10 09:15:08 +0800

    alarm,setjmp,longjmp实现带有超时特点的read操作(question.cpp问题待解决)

---
+++ sig_read_setjmp/main.cpp
@@ -0,0 +1,39 @@
+#include <setjmp.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLINE 1024
+
+static void sig_alrm(int);
+static jmp_buf env_alrm;
+
+int main(int argc, char* argv[]) {
+    
+    int n;
+    char line[MAXLINE];
+
+    if (signal(SIGALRM, sig_alrm) == SIG_ERR) {
+        printf("signal error\n");
+        return 0;
+    }
+
+    if (setjmp(env_alrm) != 0) {
+        printf("read timeout\n");
+        return 0;
+    }
+
+    alarm(5);
+    if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0) {
+        printf("read error\n");
+    }
+    alarm(0);
+
+    write(STDOUT_FILENO, line, n);
+    return 0;
+}
+
+static void sig_alrm(int signo) {
+    longjmp(env_alrm, 1);
+}
